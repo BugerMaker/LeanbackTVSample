1eedeb9328e6440c4ae3ea159249db98bc9e98f4
bugermaker <gcoder6@gmail.com>
2025-02-18 17:56:28 +0800

    添加dp转px工具类方法

--- app/src/main/java/com/bugermaker/tvapplication/utils/FontDisplayUtil.java
+++ app/src/main/java/com/bugermaker/tvapplication/utils/FontDisplayUtil.java
@@ -1,10 +1,15 @@
 package com.bugermaker.tvapplication.utils;
 
+import android.content.Context;
+
 /**
  * dp、sp 、 px之间的相互转化的工具类
  */
 public class FontDisplayUtil {
 
-
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
 
 }
